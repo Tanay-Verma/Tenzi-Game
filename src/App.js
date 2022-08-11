87e0addd1b8da7479be0868c9b73b718924d3e2c
Tanay <tanayverma98@gmail.com>
2022-08-11 12:15:32 +0530

    Project Started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='hero'>
+        <div className='hero--content'>
+          <div className='inner--content'>
+            <h1>Tenzies</h1>
+            <p>Roll until all dice are the same.
+              Click each die to freeze it at its current value between rolls.
+            </p>
+            <div className='game--grid'>
+              <p>1</p>
+              <p>2</p>
+              <p>3</p>
+              <p>4</p>
+              <p>5</p>
+              <p>6</p>
+              <p>7</p>
+              <p>8</p>
+              <p>9</p>
+              <p>10</p>
+            </div>
+            <button className='play'>Roll</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
